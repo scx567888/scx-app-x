29356d011da1a88a70f8bd7044cb9b9c03e887ca
scx567888 <scx567888@outlook.com>
2025-05-27 13:26:54 +0800

    dev

--- src/main/java/cool/scx/app/x/crud/CRUDListParam.java
+++ src/main/java/cool/scx/app/x/crud/CRUDListParam.java
@@ -3,9 +3,13 @@ package cool.scx.app.x.crud;
 import com.fasterxml.jackson.databind.JsonNode;
 import cool.scx.data.field_policy.FieldPolicy;
 import cool.scx.data.query.Query;
+import cool.scx.data.serialization.DeserializationException;
+import cool.scx.http.exception.BadRequestException;
 
-import static cool.scx.data.field_policy.serializer.FieldPolicyDeserializer.FIELD_POLICY_DESERIALIZER;
-import static cool.scx.data.query.serializer.QueryDeserializer.QUERY_DESERIALIZER;
+import static cool.scx.data.field_policy.FieldPolicyBuilder.includeAll;
+import static cool.scx.data.query.QueryBuilder.query;
+import static cool.scx.data.serialization.FieldPolicyDeserializer.deserializeFieldPolicy;
+import static cool.scx.data.serialization.QueryDeserializer.deserializeQuery;
 
 
 /**
@@ -26,12 +30,26 @@ public class CRUDListParam {
     public JsonNode extParams;
 
     public Query getQuery() {
-        return QUERY_DESERIALIZER.deserializeQuery(query);
+        if (query == null || query.isNull()) {
+            return query();
+        }
+        try {
+            return deserializeQuery(query);
+        } catch (Exception e) {
+            throw new BadRequestException("query 参数错误", e);
+        }
     }
 
     public FieldPolicy getFieldPolicy() {
+        if (fieldPolicy == null || fieldPolicy.isNull()) {
+            return includeAll();
+        }
         //我们需要去掉表达式 防止客户端 sql 注入
-        return FIELD_POLICY_DESERIALIZER.deserialize(fieldPolicy).clearAssignFields();
+        try {
+            return deserializeFieldPolicy(fieldPolicy);
+        } catch (DeserializationException e) {
+            throw new BadRequestException("fieldPolicy 参数错误", e);
+        }
     }
 
 }
