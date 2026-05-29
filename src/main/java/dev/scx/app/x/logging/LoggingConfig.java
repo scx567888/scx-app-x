59abe92e4aa9a26265c8c90e51b070a0e86eaaaa
scx567888 <scx567888@outlook.com>
2026-05-29 17:06:20 +0800

    dev

--- src/main/java/dev/scx/app/x/logging/LoggingConfig.java
+++ src/main/java/dev/scx/app/x/logging/LoggingConfig.java
@@ -2,6 +2,10 @@ package dev.scx.app.x.logging;
 
 import dev.scx.app.environment.type.ConfiguredPath;
 
+/// LoggingConfig
+///
+/// @author scx567888
+/// @version 0.0.1
 final class LoggingConfig {
 
     public String name;
